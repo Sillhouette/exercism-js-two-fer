a40058e1fb57b98c09461ad7526c57eaab45f5f5
Austin <austin@8thlight.com>
2020-01-27 15:24:44 -0800

    add first test and initial code

--- two-fer.spec.js
+++ two-fer.spec.js
@@ -1,15 +1,9 @@
 import { twoFer } from './two-fer'
 
 describe('twoFer()', () => {
-  test('no name given', () => {
+  test('twoFer() returns "One for you, one for me"', () => {
     expect(twoFer()).toEqual("One for you, one for me.")
   })
 
-  xtest('a name given', () => {
-    expect(twoFer("Alice")).toEqual("One for Alice, one for me.")
-  })
-
-  xtest('another name given', () => {
-    expect(twoFer("Bob")).toEqual("One for Bob, one for me.")
-  })
+  
 })
