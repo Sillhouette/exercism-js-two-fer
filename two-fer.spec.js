ab5b22ca07f1f33dced01c7be603b01eeb6f4c43
Austin <austin@8thlight.com>
2020-01-27 15:24:57 -0800

    add second test

--- two-fer.spec.js
+++ two-fer.spec.js
@@ -5,5 +5,8 @@ describe('twoFer()', () => {
     expect(twoFer()).toEqual("One for you, one for me.")
   })
 
-  
+  test('twoFer("Austin") returns "One for Austin, one for me"', () => {
+    expect(twoFer("Austin")).toEqual("One for Austin, one for me.")
+  })
+
 })
